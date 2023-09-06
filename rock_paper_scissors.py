4a8a2892f6bc48f0b98f0b350806d1a6b19a21ec
Louis Woodley <lwoodley@cps.edu>
2023-09-06 11:34:43 -0500

    started on rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,17 @@
 # We will write a rock paper scissors game in class - Complete it in this file
+import random
+
+
+
+choice = "rock"
+AI_choice = "paper"
+def get_choices():
+    options = ['rock', 'paper', 'scissors']
+    choice = "rock"
+    AI_choice = random.choice(options)
+    choices = {"player": choice, "computer": AI_choice}
+
+    return choices
+#I am a pro coder now (:
+choices = get_choices()
+print(choices)
