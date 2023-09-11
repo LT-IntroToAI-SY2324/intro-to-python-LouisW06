eeb17ebc08bd0206aa108b89ca709158d4b5a9ab
Louis Woodley <lwoodley@cps.edu>
2023-09-11 11:16:28 -0500

    Finished rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,15 +3,40 @@ import random
 
 
 
-choice = "rock"
-AI_choice = "paper"
 def get_choices():
     options = ['rock', 'paper', 'scissors']
-    choice = "rock"
+    choice = input("Enter a choice (rock, paper, or scissors)")
     AI_choice = random.choice(options)
     choices = {"player": choice, "computer": AI_choice}
 
     return choices
 #I am a pro coder now (:
+
+def checkwinner(player, computer):
+    print(f"You chose {player}, computer chose {computer}.")
+    if player == computer:
+        return "You tied"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock crushes scissors. You win"
+        else:
+            return "Paper engulfs rock. You lose"
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper engulfs rock. You win"
+        else:
+            return "Scissors slice paper. You lose"
+    elif player == "scissors":
+        if computer == "rock":
+            return "Rock crushes scissors. You lose"
+        else:
+            return "Scissors slice paper. You win"
+        
+
+
 choices = get_choices()
-print(choices)
+#print(choices)
+result = checkwinner(choices["player"], choices["computer"])
+print(result)
+#result = checkwinner("scissors", "paper")
+#print(result)
