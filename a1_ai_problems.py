c36a071b8380f675c839c7ea159044084127e077
Louis Woodley <lwoodley@cps.edu>
2023-09-13 11:35:10 -0500

    Duck Duck Goose

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -17,9 +17,30 @@ assert fizbuzz(5) == "Buzz", "fizzbuzz 5 test"
 assert fizbuzz(6) == "Fizz", "fizzbuzz 6 test"
 assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test"
 
-print(fizbuzz(3))
+print(fizbuzz(5))
 
 list = ["Chicken", "Hydrolic Press", "Lebron James", "Gynocolgist", "Dark Matter"]
 print(list)
 #print(list.reverse())
 print(list[1:4])
+
+
+def add_two(n):
+    m = n + 2
+    print(m)
+    return m
+
+"""assert add_two(5) == 7 
+assert add_two(-2) == 0 
+assert add_two(0) == 2"""
+
+y = add_two(2)
+
+def factorial(n: int) -> int:
+
+    result = 1
+    for x in range(1,n + 1):
+        result = result * x
+    return result
+
+print(factorial(4))
